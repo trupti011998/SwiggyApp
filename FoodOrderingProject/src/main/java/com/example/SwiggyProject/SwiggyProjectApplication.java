5548fbe57e9e985841c8a22f282bbfe8a370362a
trupti gorde <truptigorde2@gmail.com>
2022-05-16 16:01:58 +0530

    Some Packages added

---
+++ FoodOrderingProject/src/main/java/com/example/SwiggyProject/SwiggyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SwiggyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiggyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiggyProjectApplication.class, args);
+	}
+
+}
---
+++ FoodOrderingProject/src/test/java/com/example/SwiggyProject/SwiggyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SwiggyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiggyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
